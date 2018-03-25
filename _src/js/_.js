7f6df90b5a04c69c6c0894f80f669d5308fba810
Eduardo Barros <joseeduardobarros@gmail.com>
2018-03-25 20:11:02 -0300

    Update _.js

--- _src/js/_.js
+++ _src/js/_.js
@@ -1,5 +1,5 @@
 /*!
- * Elbit {ProjectName} Project v0.0.0 (http://elbit.com.br/)
- * Copyright 2013-2016 Elbit Developers
+ * Du Lago {ProjectName} Project v0.0.0 (http://elbit.com.br/)
+ * Copyright 2013-2018 Elbit Developers
  * Licensed under MIT (https://github.com/elbitdigital/base/blob/master/LICENSE)
 */
